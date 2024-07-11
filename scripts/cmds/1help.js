181773c78ba2322b266b6654cb50a409bf43e30e
Mariameae <kazukinoshita50@gmail.com>
2024-07-11 23:58:52 +0200

    Update 1help.js

--- scripts/cmds/1help.js
+++ scripts/cmds/1help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "𝙴𝚇𝚄𝙰𝚁𝚃𝙸| 📜😊";
+const doNotDelete = "𝐆𝐀𝐁𝐁𝐘| 📿🚀";
 /** 
 * @author NTKhang
 * @author: do not delete it
